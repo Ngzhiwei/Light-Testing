28cd5f04ba26d501be559e6f6e7521e5154b470c
Ngzhiwei <90157200+Ngzhiwei@users.noreply.github.com>
2021-10-14 22:58:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import serial
 Arduino = serial.Serial('COM5',9600)
 
 root = Tk() 
-root.geometry('175x210')
+root.geometry('150x210')
+
+sleep(1)
 
 cant = 0
 maxconfigc = 150
@@ -16,6 +18,7 @@ maxconfigf = 212
 celsius = 0
 kelvin = 0
 fahrenheit = 0
+stop = 0
 
 estilo = ttk.Style()
 estilo.theme_use('clam')
@@ -45,26 +48,41 @@ def Colores():
         barra2.config(style='red.Vertical.TProgressbar')
         barra3.config(style='red.Vertical.TProgressbar')
 
+def Stop():
+    global stop
+    stop = 1
+
 def HacerClick():
 
-    global cant, celsius
-    Arduino.write('10'.encode('ascii'))
+    global stop, cant, celsius
+
+    arranque.config(state=DISABLED)
+    paro.config(state=ACTIVE)
+
+    Arduino.write('1'.encode('ascii'))
     temperaturas = Arduino.readline().decode()
     Decod(temperaturas)
     Arduino.flushInput()
+
     Colores()
+
     barra1.config(value=celsius)
     barra2.config(value=fahrenheit)
     barra3.config(value=kelvin)
-    tempc.config(text=celsius)
-    tempf.config(text=fahrenheit)
-    tempk.config(text=kelvin)
+    tempc.config(text=celsius + 'ºC')
+    tempf.config(text=fahrenheit + 'ºF')
+    tempk.config(text=kelvin[0:5] + 'K')
 
-    if cant != 10:
-        root.after(1050, HacerClick)
-        cant +=1
+    if stop != 1:
+        root.after(300, HacerClick)
     else:
-        cant = 0
+        stop = 0
+        arranque.config(state=ACTIVE)
+        paro.config(state=DISABLED)
+
+    # cant +=1
+    # button.config(state=ACTIVE)
+    # cant = 0
 
 barra1 = ttk.Progressbar(root, orient='vertical', style="green.Vertical.TProgressbar", length=150, maximum=maxconfigc)
 maximobarra1 = Label(root)
@@ -77,14 +95,17 @@ tempc = Label(root, text=celsius)
 tempf = Label(root, text=fahrenheit)
 tempk = Label(root, text=kelvin)
 
-button = Button(root, text='tocame bb', command=HacerClick)
+arranque = Button(root, text='Arranque', command=HacerClick)
+paro = Button(root, text='Paro', command=Stop)
 
-button.place(y=175, x=55)
-tempc.place(y=155, x=50)
-tempf.place(y=155, x=80)
-tempk.place(y=155, x=110)
-barra1.place(y=5,x=50)
-barra2.place(y=5,x=80)
-barra3.place(y=5,x=110)
+arranque.place(y=175, x=10)
+paro.place(y=175, x=75)
+
+tempc.place(y=155, x=5)
+tempf.place(y=155, x=55)
+tempk.place(y=155, x=105)
+barra1.place(y=5,x=15)
+barra2.place(y=5,x=65)
+barra3.place(y=5,x=115)
 
 root.mainloop()
